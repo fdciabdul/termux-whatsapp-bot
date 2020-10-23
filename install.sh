68e3aa9d7786af0dfa976833084e07b8322955d9
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2020-10-23 16:08:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ apt install
 apt install wget
 apt install ffmpeg
 apt install nodejs
-apt install npm --fix-missing
-apt install chromium-browser
-apt -f install
-npm install
+npm i -g cwebp
+npm i -g ytdl
+npm i
