5b6bfe3d677d8a99a8e4c319d385005d6f1b763d
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2020-10-23 17:38:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,246 @@
-update
+const qrcode = require("qrcode-terminal");
+const moment = require("moment");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const dl = require("./lib/downloadImage.js");
+const webp = require('webp-simple-converter');
+const fetch = require('node-fetch');
+const urlencode = require("urlencode");
+const axios = require("axios");
+const
+{
+   WAConnection,
+   MessageType,
+   Presence,
+   MessageOptions,
+   Mimetype,
+   WALocationMessage,
+   WA_MESSAGE_STUB_TYPES,
+   ReconnectMode,
+   ProxyAgent,
+   waChatKey,
+} = require("@adiwajshing/baileys");
+var jam = moment().format("HH:mm");
+
+function foreach(arr, func)
+{
+   for (var i in arr)
+   {
+      func(i, arr[i]);
+   }
+}
+const conn = new WAConnection()
+conn.on('qr', qr =>
+{
+   qrcode.generate(qr,
+   {
+      small: true
+   });
+   console.log(`[ ${moment().format("HH:mm:ss")} ] Please Scan QR with app!`);
+});
+
+conn.on('credentials-updated', () =>
+{
+   // save credentials whenever updated
+   console.log(`credentials updated!`)
+   const authInfo = conn.base64EncodedAuthInfo() // get all the auth info we need to restore this session
+   fs.writeFileSync('./session.json', JSON.stringify(authInfo, null, '\t')) // save this info to a file
+})
+fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json')
+// uncomment the following line to proxy the connection; some random proxy I got off of: https://proxyscrape.com/free-proxy-list
+//conn.connectOptions.agent = ProxyAgent ('http://1.0.180.120:8080')
+conn.connect();
+
+conn.on('user-presence-update', json => console.log(json.id + ' presence is ' + json.type))
+conn.on('message-status-update', json =>
+{
+   const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
+   console.log(`${json.to}${participant} acknlowledged message(s) ${json.ids} as ${json.type}`)
+})
+
+conn.on('message-new', async(m) =>
+{
+   const messageContent = m.message
+   const text = m.message.conversation
+   let id = m.key.remoteJid
+   const messageType = Object.keys(messageContent)[0] // message will always contain one key signifying what kind of message
+   let imageMessage = m.message.imageMessage;
+   console.log(`[ ${moment().format("HH:mm:ss")} ] (${id.split("@s.whatsapp.net")[0]} => ${text}`);
+
+   if (messageType == 'imageMessage')
+   {
+      let caption = imageMessage.caption.toLocaleLowerCase()
+      const buffer = await conn.downloadMediaMessage(m) // to decrypt & use as a buffer
+      if (caption == '!sticker')
+      {
+         const stiker = await conn.downloadAndSaveMediaMessage(m) // to decrypt & save to file
+
+         const
+         {
+            exec
+         } = require("child_process");
+         exec('cwebp -q 50 ' + stiker + ' -o temp/' + jam + '.webp', (error, stdout, stderr) =>
+         {
+            let stik = fs.readFileSync('temp/' + jam + '.webp')
+            conn.sendMessage(id, stik, MessageType.sticker)
+         });
+      }
+   }
+
+   if (messageType === MessageType.text)
+   {
+      let is = m.message.conversation.toLocaleLowerCase()
+
+      if (is == '!pantun')
+      {
+
+         fetch('https://raw.githubusercontent.com/pajaar/grabbed-results/master/pajaar-2020-pantun-pakboy.txt')
+            .then(res => res.text())
+            .then(body =>
+            {
+               let tod = body.split("\n");
+               let pjr = tod[Math.floor(Math.random() * tod.length)];
+               let pantun = pjr.replace(/pjrx-line/g, "\n");
+               conn.sendMessage(id, pantun, MessageType.text)
+            });
+      }
+
+   }
+   if (text.includes("!yt"))
+   {
+      const url = text.replace(/!yt/, "");
+      const exec = require('child_process').exec;
+
+      var videoid = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+
+      const ytdl = require("ytdl-core")
+      if (videoid != null)
+      {
+         console.log("video id = ", videoid[1]);
+      }
+      else
+      {
+         conn.sendMessage(id, "gavalid", MessageType.text)
+      }
+      ytdl.getInfo(videoid[1]).then(info =>
+      {
+         if (info.length_seconds > 1000)
+         {
+            conn.sendMessage(id, " videonya kepanjangan", MessageType.text)
+         }
+         else
+         {
+
+            console.log(info.length_seconds)
+
+            function os_func()
+            {
+               this.execCommand = function (cmd)
+               {
+                  return new Promise((resolve, reject) =>
+                  {
+                     exec(cmd, (error, stdout, stderr) =>
+                     {
+                        if (error)
+                        {
+                           reject(error);
+                           return;
+                        }
+                        resolve(stdout)
+                     });
+                  })
+               }
+            }
+            var os = new os_func();
+
+            os.execCommand('ytdl ' + url + ' -q highest -o mp4/' + videoid[1] + '.mp4').then(res =>
+            {
+               conn.sendMessage(id, 'mp4/' + videoid[1] + '.mp4', MessageType.video)
+            }).catch(err =>
+            {
+               console.log("os >>>", err);
+            })
+
+         }
+      });
+
+   }
+
+
+   if (text.includes("!nulis"))
+   {
+
+      const
+      {
+         spawn
+      } = require("child_process");
+      console.log("writing...")
+      const teks = text.replace(/!nulis/, "")
+      const split = teks.replace(/(\S+\s*){1,10}/g, "$&\n")
+      const fixedHeight = split.split("\n").slice(0, 25).join("\\n")
+      console.log(split)
+      spawn("convert", [
+            "./assets/paper.jpg",
+            "-font",
+            "Indie-Flower",
+            "-size",
+            "700x960",
+            "-pointsize",
+            "18",
+            "-interline-spacing",
+            "3",
+            "-annotate",
+            "+170+222",
+            fixedHeight,
+            "./assets/result.jpg"
+         ])
+         .on("error", () => console.log("error"))
+         .on("exit", () =>
+         {
+            const buffer = fs.readFileSync("assets/result.jpg") // can send mp3, mp4, & ogg -- but for mp3 files the mimetype must be set to ogg
+
+            conn.sendMessage(id, buffer, MessageType.image)
+            console.log("done")
+         })
+   }
+
+
+   if (text.includes("!quotes"))
+   {
+      var url = 'https://jagokata.com/kata-bijak/acak.html'
+      axios.get(url)
+         .then((result) =>
+         {
+            let $ = cheerio.load(result.data);
+            var author = $('a[class="auteurfbnaam"]').contents().first().text();
+            var kata = $('q[class="fbquote"]').contents().first().text();
+
+            conn.sendMessage(
+               id,
+               `
+     _${kata}_
+        
+    
+	*~${author}*
+         `, MessageType.text
+            );
+
+         });
+   }
+
+   if (text.includes("!lirik"))
+   {
+      var nama = text.replace("!nama", "");
+      var req = urlencode(nama);
+      var url = "http://tololbgt.coolpage.biz/lirik.php?judul=" + req;
+      axios.get(url)
+         .then((result) =>
+         {
+
+            conn.sendMessage(id, result.data.replace(/pjr-enter/g, "\n"), MessageType.text);
+         });
+   }
+
+
+   // end of file
+})
