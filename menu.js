28de41528a09531c22cbde1fbd149297561a8b7b
Shelbyx <74799694+Shelbyx@users.noreply.github.com>
2020-12-05 23:53:00 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
-module.exports = {
-	menu:  "┏━┅┅┄┄⟞⟦✮⟧⟝┄┄┉┉━┓
-⠀⠀⠀⠀⠀⠀⠀MENU⠀⠀⠀⠀⠀⠀⠀
+exports.menu = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, whatsapp, kapanbotaktif, grupch1, grupch2) => {
+	return `┏━┅┅┄┄⟞⟦✮⟧⟝┄┄┉┉━┓
+ ⠀⠀⠀⠀ ∆𝚂𝚃𝚁𝙾 𝙱𝙾𝚃
 ┗━┅┅┄┄⟞⟦✮⟧⟝┄┄┉┉━┛
 
 
@@ -8,18 +8,15 @@ module.exports = {
 
 ✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
-◈▷ !admin = Menu para administradores!
+◈▷ !admin = Menu para administradores! (Talvez não esteja funcionando);
 
-◈▷ !menu1 = Menu "Divertido"👉🏻😳👈🏻
+◈▷ !sticker = Mande uma imagem com a legenda !sticker , que eu farei figurinha da sua foto;
 
-◈▷ !menu2 = Menu De Downloads (YouTube , etc.)📩
+◈▷ !yt (URL) = Mande  !yt com o link do vídeo do Youtube ao lado que irei baixar esse vídeo para você;
 
-◈▷ !menu3 = Menu Horóscopo♌
 
-◈▷ !menu4 = Menu Educação/Aprendizado📒
+━──────◅▻♢◅▻──────━
 
-✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏" ,
-	menu1: "*Bem-vindo ao Menu "Divertido"*\r\n   \r\n*!randomanime* = untuk melihat gambar anime secara random\r\n\r\n*!animehd* = untuk melihat gambar anime HD secara random v2\r\n\r\n*!quotes* : Melihat quotes dari tokoh terkenal\r\n\r\n*!pantun* : Melihat gombalan pantun pakboy\r\n\r\n*!fakta* : Melihat fakta unik secara random\r\n\r\n*!play nama lagu*\r\ncontoh: *!play whatever it takes*\r\n\r\n*tts teks* : mengubah teks menjadi suara\r\n\r\n*!wait* : Menampilkan informasi anime dengan mengirim gambar dengan caption !wait\r\n\r\n*!ptl1* : Menampilkan gambar gambar cewek cantik \uD83E\uDD29\r\n\r\n*!ptl2* : Menampilkan gambar gambar cowok ganteng \uD83D\uDE0E\r\n\r\n*!chord nama lagu* : Menampilkan Chord Gitar\r\n\r\n*!lirik nama lagu* : Menampilkan lirik lagu\r\ncontoh: *!lirik lemon tree*\r\n\r\n*!searchimage kata kunci* : Cari gambar berdasarkan kata\r\ncontoh ( _*!searchimage kata bijak*_ )",
-	menu2: "*Bem-vindo ao Menu De Downloads *\r\n   \r\n *!yt linkDoVideo* : Baixe vídeos do Youtube\r\nExemplo : !yt https:\/\/youtu.be\/K9jR4hSCbG4\r\n\r\n*!ytmp3 linkDoVideo* : Baixar MP3 do Youtube(Talvez não esteja funcionando)\r\nExemplo : !ytmp3 https:\/\/youtu.be\/xUVz4nRmxn4\r\n\r\n*!fb linkDoVideo* : Baixe vídeos do Facebook\r\nExemplo : !fb linkDoVideo\r\n\r\n*!ig linkDoVideo* : Baixe fotos e vídeos do Instagram\r\nExemplo : !ig linkDoVideo\r\n\r\n*!pin linkDoVideo* : Baixe um vídeo do Pinterest\r\nExemplo : !pin linkDoVideo",
-	menu3: "*!nama* : Veja o significado do seu nome (irá vir em indonésio)\r\n Exemplo : !nama Bondan\r\n\r\n*!pasangan* : Verifique a probabilidade do Shipp (Irá vir em indonésio)\r\n Exemplo : !pasangan Dimas & Dinda"
+◈▷ Grupo Do Bot : https://chat.whatsapp.com/Gk1M5x5XhQ0GeVLSEG7AEg
+`
 }
